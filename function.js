d7d50f5114d3e2541e30374510e02dcffc34869d
RajeshPandey10 <imrajesh2005@gmail.com>
2025-11-17 15:32:40 +0545

    function complete

---
+++ function.js
@@ -0,0 +1,48 @@
+// const a =10;
+// const b=20;
+
+// function sum(a,b){
+//     console.log(a**b)
+// }
+
+// sum(2,3)
+
+// const x =50;
+// const y=20;
+// const sum1 =x+y;
+// console.log(sum1)
+
+
+//function types
+// function name(){
+//     console.log("hello")
+// }
+// name()
+
+// let name = function hello(){
+
+// }
+
+// ()={
+
+// }
+//arrow function
+// function sum(a,b){
+//     console.log(a**b)
+// }
+
+const hello =(a=20,b=20)=>{
+const sum =a+b
+if(sum>30){
+   console.log("this is sum")
+   return sum
+}
+
+}
+hello(50,20)
+
+
+
+
+
+
