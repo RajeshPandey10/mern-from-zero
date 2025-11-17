a732e037a967ef224579c030e72b77b28c7d0d99
RajeshPandey10 <imrajesh2005@gmail.com>
2025-11-17 15:12:34 +0545

    loops and function class

---
+++ loop.js
@@ -0,0 +1,20 @@
+
+// for (let i=1;i<=10;i++){
+//     console.log(i+5)
+// }
+
+let i=0;
+while( i<10){
+    
+    i=i+1;
+    console.log(i)
+}
+
+do{
+    i=i+1;
+    console.log(i) 
+}while( i<10)
+
+
+
+
