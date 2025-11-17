6c71c740b3995b7a634898c76852266796d265e2
RajeshPandey10 <imrajesh2005@gmail.com>
2025-11-17 14:57:04 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,26 @@
+//conditional statement
+
+const distance =400;
+
+// if(temp>=30){
+//  console.log("temp is high ..please turn the fan on")
+//  return
+
+// }
+// else if(temp>20 && temp<30){
+//     console.log("temp is good ..")
+// }
+// else{
+//     console.log("hi")
+// }
+
+
+//ternary operator 
+// const result =distance<500?"walk":"go with bus"
+
+// console.log(result)
+
+//sort circuit
+const result =distance>300&&"go by bus"
+console.log(result)
+    
